880d4ad070a55f88a23165135921d738e27dd12d
Matias Millalonco <mati.mila30@gmail.com>
2023-06-02 15:45:53 -0300

    Implementación de métodos para conectar con otra WebAPI

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RRHHAPI.Helpers;
+
+namespace RRHHApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+           private readonly DBConnection _db = new DBConnection();
+        [HttpGet(Name = "GetCandidatos")]
+
+        public async Task<ActionResult> GetCandidatos()
+        {
+            try
+            {
+            /* https://localhost:44355/Historia?dniAlumno=12345678 */
+                HttpApiHelper helper = new HttpApiHelper();
+                var endpoint = "/Historia";
+                endpoint += "?dniAlumno=12345678";
+                return await helper.SendAsync(HttpMethod.Get, "https://localhost:44355", endpoint, null);
+            } catch(Exception e)
+            {
+                throw;
+            }
+        }
+
+    }
+}
